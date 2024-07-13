e2a53b0d4615aa9eb8a4352ecbefa9513c057c91
Leonel Donati <leonelroman321@gmail.com>
2024-07-13 16:10:48 -0300

    fixed format

--- src/pages/api/add-post.ts
+++ src/pages/api/add-post.ts
@@ -1,5 +1,6 @@
-import { postsModel } from "@db";
+import { postsModel, usersModel } from "@db";
 import type { APIRoute } from "astro";
+import { ObjectId } from "mongodb";
 import { resJson } from "src/helpers/response";
 import { uploadFile } from "src/libs/cld";
 import { optimizePostAsset } from "src/libs/sharp";
@@ -7,7 +8,7 @@ import { optimizePostAsset } from "src/libs/sharp";
 export const POST: APIRoute = async ({ request }) => {
   const form = await request.formData();
   const content = form.get("content")?.toString();
-  const images = form.getAll("images") as File[] | [''];
+  const images = form.getAll("images") as File[] | [""];
   const userId = form.get("userId")?.toString();
   if (!content) return resJson({ message: "Wrong content!." }, { status: 400 });
 
@@ -15,7 +16,7 @@ export const POST: APIRoute = async ({ request }) => {
     let uploadedFiles = null;
 
     // si hay más de una imagen
-    if (images[0] !== '' && images.length > 1) {
+    if (images[0] !== "" && images.length > 1) {
       const promises = images
         .map(
           async (file) =>
@@ -33,30 +34,40 @@ export const POST: APIRoute = async ({ request }) => {
     }
 
     // si hay una sola imagen
-    if (images[0] !== '' && images.length === 1) {
+    if (images[0] !== "" && images.length === 1) {
       const filePath = (
         await optimizePostAsset(Buffer.from(await images[0].arrayBuffer()), {
           folder: "posts",
         })
       )?.filePath;
-      const uploadedFile = (await uploadFile(filePath, { folder: "posts" }))?.data;
+      const uploadedFile = (await uploadFile(filePath, { folder: "posts" }))
+        ?.data;
 
       uploadedFiles = [uploadedFile];
-    } 
+    }
     
     const postSchema = {
       content,
       userId,
+      userOwner: await usersModel.findOne({ _id: new ObjectId(userId) }),
       createdAt: new Date(Date.now()),
       updatedAt: new Date(Date.now()),
-      images: uploadedFiles
-        ? uploadedFiles
-        : [],
-    }
+      images: uploadedFiles ? uploadedFiles : [],
+      likes: [],
+      comments: [],
+    };
+    
+    
+    const updatedUser = await usersModel.findOneAndUpdate(
+      { _id: new ObjectId(userId) },
+      { $push: { posts: postSchema as never } },
+      { returnDocument: "after" }
+    );
+    
+    await postsModel.insertOne(postSchema);
 
-    await postsModel.insertOne(postSchema)
 
-    return resJson({ message: "Posted!" });
+    return resJson({ message: "Posted!", updatedUser });
   } catch (e) {
     return resJson({ message: "Error on server." });
   }
