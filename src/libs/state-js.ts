c45787a0a93729eb90eb399d2d9f361b18cd5f01
Leonel Donati <leonelroman321@gmail.com>
2024-07-13 14:35:05 -0300

    fixed persist state handler

--- src/libs/state-js.ts
+++ src/libs/state-js.ts
@@ -1,6 +1,6 @@
 import { readFile, writeFile } from "fs/promises";
 type CreateState = <State>(
-  state: State,
+  statePayload: State,
   options?: { persist: boolean }
 ) => {
   get: () => Promise<State>;
@@ -12,35 +12,39 @@ const createState: CreateState = (statePayload, options) => {
   let state;
   state = statePayload;
 
+  if (options?.persist) {
+    writeFile("./src/stores/state.txt", JSON.stringify(state), {}).then(() => {})
+  }
+
   const get = async () => {
     if (options?.persist) {
       try {
         const savedState = await readFile("./src/stores/state.txt", "utf8");
-
+        
         return JSON.parse(savedState);
       } catch (err) {
         return console.error(err);
       }
     }
-
+    
     listen();
     return state;
   };
-
+  
   const set = async (newState) => {
     state = newState;
-
+    
     listen();
-
+    
     if (options?.persist) {
       try {
-        await writeFile("./src/stores/state.txt", JSON.stringify(newState), {});
+        await writeFile("./src/stores/state.txt", JSON.stringify(state), {});
       } catch (e) {
         console.log(e);
       }
     }
   };
-
+  
   const listen = (cb?) => {
     return cb && cb(state);
   };
