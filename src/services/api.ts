419c751e5d6605166ede9413c16c1a1f99b3c120
Leonel Donati <leonelroman321@gmail.com>
2024-07-23 19:45:15 -0300

    fixed bug on dynamic url origin.

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,32 +1,41 @@
-async function getPosts(q?: number) {
-  try {
-    const res = await fetch(`http://localhost:4321/api/get-post?q=${q ?? 15}`);
 
-    return await res.json();
-  } catch (e) {
-    console.log(e);
+
+function ApiFunctions (origin: string) {
+  async function getPosts(q?: number) {
+    try {
+      const res = await fetch(`${origin}/api/get-post?q=${q ?? 15}`);
+  
+      return await res.json();
+    } catch (e) {
+      console.log(e);
+    }
+  }
+  
+  async function getUser(id: string) {
+    try {
+      const res = await fetch(`${origin}/api/get-user?id=${id}`);
+  
+      return await res.json();
+    } catch (e) {
+      console.log(e);
+    }
+  }
+  
+  async function getUserPosts(id: string) {
+    try {
+      const res = await fetch(
+        `${origin}/api/get-user-posts?id=${id}`
+      );
+  
+      return await res.json();
+    } catch (e) {
+      console.log(e);
+    }
+  }
+  return {
+    getPosts,
+    getUser,
+    getUserPosts
   }
 }
-
-async function getUser(id: string) {
-  try {
-    const res = await fetch(`http://localhost:4321/api/get-user?id=${id}`);
-
-    return await res.json();
-  } catch (e) {
-    console.log(e);
-  }
-}
-
-async function getUserPosts(id: string) {
-  try {
-    const res = await fetch(
-      `http://localhost:4321/api/get-user-posts?id=${id}`
-    );
-
-    return await res.json();
-  } catch (e) {
-    console.log(e);
-  }
-}
-export { getPosts, getUser, getUserPosts };
+export default ApiFunctions;
