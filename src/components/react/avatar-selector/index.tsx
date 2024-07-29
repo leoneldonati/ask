fe5db885175df73c260e3809209b8a9f24f1147d
Leonel Donati <leonelroman321@gmail.com>
2024-07-29 20:30:15 -0300

    added svg

--- src/components/react/avatar-selector/index.tsx
+++ src/components/react/avatar-selector/index.tsx
@@ -25,10 +25,27 @@ export default function AvatarSelector() {
 
       <img
         src={selectedAvatar}
-        hidden={selectedAvatar === ''}
+        hidden={selectedAvatar === ""}
         className="sign__label--avatar selected-avatar"
         loading="lazy"
       />
+
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="24"
+        height="24"
+        viewBox="0 0 24 24"
+        fill="currentColor"
+        stroke="currentColor"
+        strokeWidth="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        style={{ display: selectedAvatar === '' ? 'auto' : 'none' }}
+      >
+        <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+        <path d="M12 5l0 14"></path>
+        <path d="M5 12l14 0"></path>
+      </svg>
     </label>
   );
 }
