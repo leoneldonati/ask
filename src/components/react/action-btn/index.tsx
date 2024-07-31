c02583ec095cb240b10beea3d66ad814ca178c47
Leonel Donati <leonelroman321@gmail.com>
2024-07-31 16:40:38 -0300

    fixed styles on action btn

--- src/components/react/action-btn/index.tsx
+++ src/components/react/action-btn/index.tsx
@@ -1,3 +1,4 @@
+import type { UserLogged } from "@types";
 import "./index.css";
 import { useEffect, useState } from "react";
 
@@ -12,7 +13,6 @@ type Props = {
     ownerName: string;
   };
   length: number;
-  children;
 };
 export default function ActionBtn(props: Props) {
   const { action, length: initialLength, payload } = props;
@@ -35,10 +35,8 @@ export default function ActionBtn(props: Props) {
 
     if (action === "like" && hasLiked)
       return setStyles({
-        color: "#fff",
-        background:
-          "linear-gradient(to top, var(--color_accent), var(--color_accent_light))",
-        boxShadow: "0 5px 2px var(--color_accent_light)",
+        color: "var(--color_accent)",
+        fill: "var(--color_accent)",
       });
   }
 
@@ -56,6 +54,11 @@ export default function ActionBtn(props: Props) {
   }
 
   async function sendLike() {
+    setStyles({
+      color: "var(--color_accent)",
+      fill: "var(--color_accent)",
+    });
+    setTitle("Liked!");
     try {
       const res = await fetch(
         `http://localhost:4321/api/post-actions?type=${action}&from=${payload?.from}&to=${payload?.to}`,
@@ -68,29 +71,19 @@ export default function ActionBtn(props: Props) {
       const data = await res.json();
 
       const hasLiked = data?.post?.likes?.find((id) => payload?.from === id);
-      const newLength = Number(data?.post?.likes?.length);
 
-      if (hasLiked) {
-        setStyles({
-          color: "#fff",
-          background:
-            "linear-gradient(to top, var(--color_accent), var(--color_accent_light))",
-          boxShadow: "0 5px 2px var(--color_accent_light)",
-        });
-        setTitle("Liked!");
-        return setLength(newLength);
+      if (!hasLiked) {
+        setTitle("Like this post!");
+        setLength(length - 1);
+        return setStyles({ fill: "none" });
       }
-      setTitle("Like this post!");
-      setLength(newLength);
-      setStyles({});
+      setLength(length + 1);
     } catch (e) {}
   }
 
   useEffect(() => {
     handleStyles();
     handleTitle();
-
-    console.log(props)
   }, []);
   return (
     <>
@@ -100,9 +93,24 @@ export default function ActionBtn(props: Props) {
           className="action-btn"
           style={styles}
           title={title}
-          onClick={async () => await sendLike()}
         >
-          {props.children}
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="currentColor"
+            stroke-width="2"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+          >
+            <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+            <path d="M7 10h10" />
+            <path d="M9 14h5" />
+            <path d="M12.4 3a5.34 5.34 0 0 1 4.906 3.239a5.333 5.333 0 0 1 -1.195 10.6a4.26 4.26 0 0 1 -5.28 1.863l-3.831 2.298v-3.134a2.668 2.668 0 0 1 -1.795 -3.773a4.8 4.8 0 0 1 2.908 -8.933a5.33 5.33 0 0 1 4.287 -2.16" />
+          </svg>
+
           <span className="likes">{length}</span>
         </a>
       ) : (
@@ -112,7 +120,20 @@ export default function ActionBtn(props: Props) {
           title={title}
           onClick={async () => await sendLike()}
         >
-          {props.children}
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+            stroke-width="2"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            style={{ fill: !hasLiked && "none" }}
+          >
+            <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+            <path d="M19.5 12.572l-7.5 7.428l-7.5 -7.428a5 5 0 1 1 7.5 -6.566a5 5 0 1 1 7.5 6.572"></path>
+          </svg>
           <span className="likes">{length}</span>
         </button>
       )}
