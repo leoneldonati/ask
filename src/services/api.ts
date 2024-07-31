5364664cf08c764de5a328c991698458ae25d0e7
Leonel Donati <leonelroman321@gmail.com>
2024-07-31 14:48:16 -0300

    fixed apifn

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,32 +1,28 @@
-
-
-function ApiFunctions (origin: string) {
+function ApiFunctions(origin: string) {
   async function getPosts(q?: number) {
     try {
       const res = await fetch(`${origin}/api/get-post?q=${q ?? 15}`);
-  
+
       return await res.json();
     } catch (e) {
       console.log(e);
     }
   }
-  
+
   async function getUser(id: string) {
     try {
       const res = await fetch(`${origin}/api/get-user?id=${id}`);
-  
+
       return await res.json();
     } catch (e) {
       console.log(e);
     }
   }
-  
+
   async function getUserPosts(id: string) {
     try {
-      const res = await fetch(
-        `${origin}/api/get-user-posts?id=${id}`
-      );
-  
+      const res = await fetch(`${origin}/api/get-user-posts?id=${id}`);
+
       return await res.json();
     } catch (e) {
       console.log(e);
@@ -35,7 +31,7 @@ function ApiFunctions (origin: string) {
   return {
     getPosts,
     getUser,
-    getUserPosts
-  }
+    getUserPosts,
+  };
 }
 export default ApiFunctions;
