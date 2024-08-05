5e9059e61b1bea80ce183f9a7004c501f38f8310
Leonel Donati <leonelroman321@gmail.com>
2024-08-05 17:37:27 -0300

    fixed helper parseDate to improve more UX

--- src/helpers/date.ts
+++ src/helpers/date.ts
@@ -1,8 +1,7 @@
 function parseDate (date: Date | string) {
   const isToday = new Date().getDay() === new Date(date).getDay()
   const hours = new Date(date).getHours()
-  const minutes = new Date(date).getMinutes()
-  if (isToday) return `Today, ${hours}:${minutes} hs.`
+  if (isToday) return `Today, ${hours} hs.`
 
   return new Date(date).toLocaleDateString('es')
 }
